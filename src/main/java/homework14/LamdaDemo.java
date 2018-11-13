e9592779b2f97bc260dc4eccbf3d9ec85cbabc6a
IrinaKuzzmina <kuzmina.irina.acc@gmail.com>
2018-11-13 05:51:30 -0500

    Homework 14 updated

--- src/main/java/homework14/LamdaDemo.java
+++ src/main/java/homework14/LamdaDemo.java
@@ -1,4 +1,4 @@
-package homework14;
+package src.main.java.homework14;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,10 +20,11 @@ public class LamdaDemo {
     }
     public static void main(String[] args) {
 
-        //2) оставьте лямбда-выражение, которое возвращает значение true, если
+        //1) оставьте лямбда-выражение, которое возвращает значение true, если
         //число принадлежит к диапазону чисел 10-20, включая граничные значения
 
         boolean isInRange = checkNumber(10, num -> num >= 10 && num <= 20);
+        System.out.println("Does 10 is bigger or equal to 10 and less or equal to 20");
         System.out.println(isInRange);
 
         String initStr = "This is initial string";
@@ -32,9 +33,12 @@ public class LamdaDemo {
         //3)  Создайте лямбда-выражение, которое переводит строку в верхний режим.
         //Перевести строку в верхний регистр с помощью лямбда-выражения.
         String stringToUpper = changeStr(str -> str.toUpperCase(), initStr);
+        System.out.println("*****String to upper case*****");
         System.out.println(stringToUpper);
+
         //Перевести строку в верхний регистр с помощью ссылки на метод.
         String stringToUpper2 = changeStr(String::toUpperCase, initStr);
+        System.out.println("*****String to upper case var 2*****");
         System.out.println(stringToUpper2);
 
         //4) Заданную строк разбить на слова
@@ -43,15 +47,20 @@ public class LamdaDemo {
             List<String> stringSplittedTemp = new ArrayList<>(Arrays.asList(str.split(" ")));
             return stringSplittedTemp;
         }, initStr);
+        System.out.println("*****Splitted String*****");
         System.out.println(stringSplitted);
 
-        //Сортировать по алфавит
+
+        //4) - отсортировать слова по алфавиту не учитывая регистр первой буквы
 
 
+        stringSplitted.sort(String::compareToIgnoreCase);
+        System.out.println("*****Sorted by alphabet*****");
+        System.out.println(stringSplitted);
 
+        //4)  - отсортировать слова по длине
 
-        List<String> strings = new ArrayList<>(Arrays.asList("three", "one", "two", "four"));
-        strings.sort((word1, word2) -> {
+        stringSplitted.sort((word1, word2) -> {
             if (word1.length() > word2.length()) {
                 return 1;
             } else if (word1.length() == word2.length()) {
@@ -59,9 +68,14 @@ public class LamdaDemo {
             } else return -1;
 
         });
+        System.out.println("*****Sorted by length*****");
+        System.out.println(stringSplitted);
 
-        System.out.println(strings);
-    }
+        stringSplitted.sort(Comparator.comparing(String::length));
+        System.out.println("*****Sorted by length vr2****");
+        System.out.println(stringSplitted);    }
+
+
 
     private static boolean checkNumber(int number, Predicate<Integer> checker) {
         return checker.test(number);
